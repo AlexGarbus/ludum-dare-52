11b4de91aa939642261d3e634f53e8d56358b10b
AlexGarbus <alex.garbus@outlook.com>
2023-01-07 12:05:31 -0500

    Weapon cooldown

--- objects/combat/weapon/Weapon.cs
+++ objects/combat/weapon/Weapon.cs
@@ -5,12 +5,16 @@ namespace LudumDare52.Objects.Combat
 {
     public class Weapon : Position2D
     {
+        [Signal]
+        delegate void ShotFired();
+
         [Export]
         private readonly Vector2 _aimDirection;
 
         [Export]
         private readonly Shot _startShot;
 
+        private bool _canShoot = true;
         private Shot _currentShot;
 
         public override void _Ready()
@@ -20,6 +24,11 @@ namespace LudumDare52.Objects.Combat
 
         public void Shoot()
         {
+            if (!_canShoot)
+            {
+                return;
+            }
+
             PhysicsArea[] projectiles = _currentShot.Instance();
             foreach (PhysicsArea projectile in projectiles)
             {
@@ -28,7 +37,10 @@ namespace LudumDare52.Objects.Combat
                 // TODO: Set collision mask
             }
 
-            // TODO: Cooldown
+            _canShoot = false;
+            EmitSignal(nameof(ShotFired));
         }
+
+        public void OnCooldownTimeout() => _canShoot = true;
     }
 }
