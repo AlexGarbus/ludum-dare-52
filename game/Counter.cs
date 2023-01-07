3597824bbeadb072caf492fea3cf804ba2140aa9
AlexGarbus <alex.garbus@outlook.com>
2023-01-06 20:36:05 -0500

    Score

---
+++ game/Counter.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+namespace LudumDare52.Game
+{
+    public class Counter : Node
+    {
+        [Signal]
+        delegate void Changed(int count);
+
+        public int Count 
+        {
+            get { return _count; } 
+            private set
+            {
+                _count = value;
+                EmitSignal(nameof(Changed), _count);
+            }
+        }
+
+        private int _count = 0;
+
+        public void Add(int value)
+        {
+            Count += value;
+        }
+    }
+}
