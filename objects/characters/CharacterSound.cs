dcda4a7321d17a502acddce87e341b53db4c79cf
AlexGarbus <alex.garbus@outlook.com>
2023-01-07 20:59:30 -0500

    CharacterSound

---
+++ objects/characters/CharacterSound.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+namespace LudumDare52.Objects.Characters
+{
+    public class CharacterSound : Node
+    {
+        private AudioStreamPlayer _damage;
+        private AudioStreamPlayer _destroy;
+
+        public override void _Ready()
+        {
+            _damage = GetNode<AudioStreamPlayer>("%DamageSound");
+            _destroy = GetNode<AudioStreamPlayer>("%DestroySound");
+        }
+
+        public void OnHealthChanged(int current, int previous)
+        {
+            if (current == 0)
+            {
+                _destroy.Play();
+            }
+            else if (current - previous < 0)
+            {
+                _damage.Play();
+            }
+        }
+    }
+}
