51b41055ab097dd4bef4245887720c70211f9cbc
AlexGarbus <alex.garbus@outlook.com>
2023-01-14 18:00:12 -0500

    GameScore

--- game/Game.cs
+++ game/Game.cs
@@ -20,18 +20,13 @@ namespace LudumDare52.Game
         [Export(PropertyHint.File, "*.tscn")]
         private readonly string _introScenePath;
 
-        [Export(PropertyHint.Range, "1,100,or_greater")]
-        private readonly int _enemyScore = 100;
-
         private bool _exited = false; // TODO: Share with Intro.
         private State _state = State.RUNNING;
 
-        private Counter _score; // TODO: Move to GameScore node.
         private Timer _endTimer;
 
         public override void _Ready()
         {
-            _score = GetNode<Counter>("%Score");
             _endTimer = GetNode<Timer>("%EndTimer");
             InitializeUserInterface();
             GD.Randomize();
@@ -56,33 +51,10 @@ namespace LudumDare52.Game
             }
         }
 
-        public void OnEnemyHealthChanged(int current, int previous)  // TODO: Move to GameScore node.
-        {
-            if (current == 0)
-            {
-                _score.Add(_enemyScore);
-            }
-        }
-
-        public void OnEnemySpawned(Character enemy)  // TODO: Move to GameScore node.
-        {
-            enemy.GetNode<Health>("%Health").Connect("Changed", this, nameof(OnEnemyHealthChanged));
-        }
-
         public void EndGame()
         {
             _state = State.OVER;
-            EvaluateScore();
             EmitSignal(nameof(Ended));
-        }
-
-        private void EvaluateScore()  // TODO: Move to GameScore node.
-        {
-            int bestScore = SaveLoad.Load();
-            if (_score.Count > bestScore)
-            {
-                SaveLoad.Save(_score.Count);
-            }
         }
 
         private void InitializeUserInterface() // TODO: Pass Player and GameScore into HeadsUpDisplay.Initialize() instead.
